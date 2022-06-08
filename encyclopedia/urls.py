a2aab30444af46e2b4f06e0f345b0e7747c132e6
Lauren Mizner <lmizner@hotmail.com>
2022-06-07 19:24:45 -0700

    Updated "random" function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("wiki/random", views.random, name="random"),
+    path("wiki/random/", views.random, name="random"),
 ]
 
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 from . import util
 from markdown2 import Markdown
 from random import randint
+from random import choice
 
 # Form to create new entry pages
 class NewEntryForm(forms.Form):
@@ -30,12 +31,15 @@ def index(request):
 # Display a specific entry page
 def entry(request, title):
     markdowner = Markdown()
+
+    # Retrieve entry page by title
     entry_page = util.get_entry(title)
 
     # Check if title page exists
     # If no title page exists, display the error page
     if entry_page is None:
         return render(request, "encyclopedia/error.html")
+        
     # If the title page exists, display the title of the entry and converted markdown content
     else:
         return render(request, "encyclopedia/entry.html", {
@@ -47,14 +51,17 @@ def entry(request, title):
 # Returns an entry page at random
 def random(request):
     markdowner = Markdown()
+
     # Obtain list of all entries
     all_entries = util.list_entries()
 
-    # Use random function to pull a random entry from the list
-    entry = randint(0, len(all_entries)-1)
-    random_entry = all_entries[entry] 
+    # Use choice funtion to select a random entry from the list
+    random_entry = choice(all_entries)
+
+    # Retrieves the encyclopedia entry corresponding to the "random_entry" title
     entry_page = util.get_entry(random_entry)
     
+    # Renders the screen with entry title and contents
     return render(request, "encyclopedia/random.html", {
         "entry_page": markdowner.convert(entry_page),
         "entry_title": random_entry
